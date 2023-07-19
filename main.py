4376c9ecd54d91acfa3454774a2ef1a1cd575300
Michał Oleksy <michaloleksy97@gmail.com>
2023-07-20 00:38:31 +0200

    Add argument parsing and simple logging

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import argparse
+import functools
 import logging
 import re
 from PIL import Image
@@ -19,20 +21,23 @@ def is_bitstream_finished(terminated_bitstream):
     )
 
 
-def encode_message_in_image(image, message, offset):
+def encode(image, offset, message):
     message_bitstream = split_message_into_bitstream(message)
     data_bitstream = message_bitstream + BISTREAM_TERMINATOR
     data_bitstream_length = len(data_bitstream)
-    logger.debug(f'Data bitstream length: {data_bitstream_length}')
+    logger.debug(f'Data bitstream: {data_bitstream}, '
+                 f'length: {data_bitstream_length}')
     width, height = image.size
     logger.debug(f'Image dimensions: {image.size}')
     image_length = width * height // 8
     if image_length < data_bitstream_length:
         raise Exception('The message will never fit in the image')
     elif offset > image_length:
-        raise Exception(f'The offset {offset} exceeds image length {image_length}')
+        raise Exception(f'The offset {offset} exceeds image length '
+                        f'{image_length}')
     elif image_length - offset < data_bitstream_length:
-        raise Exception(f'The message will not fit in the image at offset {offset}')
+        raise Exception(f'The message will not fit in the image '
+                        f'at offset {offset}')
     pixels = image.load()
     changed_bits_count = 0
     for bit_index, bit in enumerate(data_bitstream):
@@ -42,49 +47,96 @@ def encode_message_in_image(image, message, offset):
         modified_b = b // 2 * 2 + int(bit)
         pixels[x, y] = (r, g, modified_b)
         if b != modified_b:
-            logger.debug((f'{bit_index}/{data_bitstream_length}: '
-                          f'rgb{(r, g, b)} -> rgb{(r, g, modified_b)}'));
-            changed_bits_count += 1;
-    logger.debug(f'{changed_bits_count} bits changed ({round(changed_bits_count / data_bitstream_length * 100, 2)} %)')
+            logger.debug(f'{bit_index}/{data_bitstream_length}: '
+                         f'rgb{(r, g, b)} -> rgb{(r, g, modified_b)}')
+            changed_bits_count += 1
+    changed_bits_ratio = round(changed_bits_count / data_bitstream_length * 100,
+                               2)
+    logger.debug(f'{changed_bits_count}/{data_bitstream_length} bits changed '
+                 f'({changed_bits_ratio} %)')
     return data_bitstream
 
 
-def decode_message_from_image(image, offset):
+def encode_message_in_image(image_path, offset, message, output_image_path):
+    logger.info(f'Encoding message `{message}` at offset '
+                f'{hex(offset)} in {image_path}')
+    with Image.open(image_path) as image:
+        encode(image, offset, message)
+        image.save(output_image_path)
+
+
+def decode(image, offset):
     width, height = image.size
+    logger.debug(f'Image dimensions: {image.size}')
     image_length = width * height // 8
     if offset > image_length:
-        raise Exception(f'The offset {offset} exceeds image length {image_length}')
+        raise Exception(f'The offset {offset} exceeds '
+                        f'image length {image_length}')
     pixels = image.load()
     data_bitstream = ''
     bit_index = 0
-    while not is_bitstream_finished(data_bitstream):
+    while True:
+        if is_bitstream_finished(data_bitstream):
+            logger.debug(f'Message terminated')
+            break
         x = (bit_index + offset) % width
         y = (bit_index + offset) // width
         b = pixels[x, y][2]
         bit = b & 1
         data_bitstream += str(bit)
+        logger.debug(f'msg[{bit_index}] = {bit}')
         bit_index += 1
+    logger.debug(f'Message bitstream: {data_bitstream}, length: {bit_index}')
     message_bitstream = data_bitstream[:-8]
+    message_bitstream_length = len(message_bitstream)
     return ''.join(chr(int(byte, 2)) for byte in re.findall('.{8}', message_bitstream))
 
 
+def decode_message_from_image(image_path, offset):
+    logger.info(f'Decoding message at offset {hex(offset)} from {image_path}')
+    with Image.open(image_path) as image:
+        message = decode(image, offset)
+        logger.info(f'Decoded message: {message}')
+
+
 def configure_logging():
-    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.WARNING) # supress PIL debug logging
+    # suppress PIL DEBUG logs by globally setting WARNING logging level
+    logging.basicConfig(format='%(levelname)s: %(message)s',
+                        level=logging.WARNING)
     logger.setLevel(logging.DEBUG)
 
 
 if __name__ == '__main__':
+    main_parser = argparse.ArgumentParser()
+    main_parser.add_argument(
+        'image_path',
+        help='Path to image used to encode/decode message'
+    )
+    main_parser.add_argument(
+        'offset',
+        type=functools.partial(int, base=0),
+        help='Message position in image file'
+    )
+    subparsers = main_parser.add_subparsers(
+        title='Actions',
+        description='Available actions to perform',
+        required=True
+    )
+    encode_parser = subparsers.add_parser('encode')
+    encode_parser.add_argument(
+        'message',
+        help='Message to encode in image file'
+    )
+    encode_parser.add_argument(
+        'output_image_path',
+        help='Path to resulting image with encoded message'
+    )
+    encode_parser.set_defaults(func=encode_message_in_image)
+    decode_parser = subparsers.add_parser('decode')
+    decode_parser.set_defaults(func=decode_message_from_image)
+    args = main_parser.parse_args()
+
     configure_logging()
-    test_message = 'test message'
-
-    logger.info(f'Message to encode: {test_message}')
-
-    with Image.open('ee.png') as image:
-        bitstream = encode_message_in_image(image, test_message, 0x100)
-        image.save('ee2.png')
-
-    with Image.open('ee2.png') as image:
-        message = decode_message_from_image(image, 0x100)
-
-    logger.info(f'Encoded message as bitstream: {bitstream}')
-    logger.info(f'Decoded message: {message}')
+    func = args.func
+    del args.func
+    func(**vars(args))
